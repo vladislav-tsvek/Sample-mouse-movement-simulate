8c9f65b95040afc5c6e36906f47d42bfecc5409d
vladislav.tsvek <vladislav.tsvek@V-TSVEK>
2016-09-15 16:12:35 +0300

    Add project files.

---
+++ Sample mouse movement simulate/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Sample_mouse_movement_simulate
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
