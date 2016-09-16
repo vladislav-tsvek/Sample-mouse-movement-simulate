43e0f59b95e8108be72bc930fbd4191a91538810
vladislav.tsvek <vladislav.tsvek@teamdev.com>
2016-09-16 13:25:18 +0300

    Mouse movement by timer redrawing

--- Sample mouse movement simulate/MainWindow.xaml.cs
+++ Sample mouse movement simulate/MainWindow.xaml.cs
@@ -1,19 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
 using System.Runtime.InteropServices;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Forms;
+using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Sample_mouse_movement_simulate
 {
@@ -27,42 +15,89 @@ namespace Sample_mouse_movement_simulate
        private static extern bool SetCursorPos(int X, int Y);
 
         static int coordX;
-        static int coordY = 0;
+        static int coordY;
+
+        static int baseX;
+        static int baseY;
+
+        int tempX;
+        int tempY;
+
+        System.Windows.Forms.Timer myTimer;
 
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void moveMS_Click(object sender, RoutedEventArgs e)
+        private void moveMS_Click(object sender, RoutedEventArgs e1)
         {
-          
-            //if ((main.Width >= coordX) && (main.Height >= coordY))
-            //{
-                SetCursor(Convert.ToInt16(positionX.Text), Convert.ToInt16(positionY.Text));
-            //}
-            //else
-            //{
-            //    status.Content = "Out of Window";
-            //}
-                   
+            baseX = (int)Mouse.GetPosition(main).X;
+            baseY = (int)Mouse.GetPosition(main).Y;
+
+            coordX = Convert.ToInt16(positionX.Text);
+            coordY = Convert.ToInt16(positionY.Text);
+            
+            tempX = baseX;
+            tempY = baseY;
+            
+            myTimer = new System.Windows.Forms.Timer();
+            myTimer.Interval = 10;
+            myTimer.Tick += MyTimer_Tick;
+           
+            myTimer.Start();           
+        }
+
+        private void MyTimer_Tick(object sender, EventArgs e)
+        {
+
+            if (coordX > tempX)
+                tempX = tempX + 1;
+            else if (coordX == tempX){}
+            else
+                tempX = tempX - 1;
+
+
+            if (coordY > tempY)
+                tempY = tempY + 1;
+            else if (coordY == tempY) {}
+            else
+                tempY = tempY - 1;
+
+            if ((coordX == tempX) && (coordY == tempY))
+            {
+                myTimer.Stop();
+                status.Content = "Done!";
+            }
+            else
+            {
+                SetCursor(tempX, tempY);
+            }
         }
 
         private static void SetCursor(int x, int y)
         {
-            // Left boundary
-            var xL = (int)App.Current.MainWindow.Left + 8;
-            // Top boundary
-            var yT = (int)App.Current.MainWindow.Top + 31;
-            coordX = x + xL;
-            coordY = y + yT;
+            try
+            {
+                // Left boundary
+                var xL = (int)App.Current.MainWindow.Left + 8;
+                // Top boundary
+                var yT = (int)App.Current.MainWindow.Top + 31;
+                //coordX = x + xL;
+                //coordY = y + yT;
 
-            SetCursorPos(x + xL, y + yT);
+                SetCursorPos(x + xL, y + yT);
+            }
+           
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine(e);
+            }
         }
 
         private void main_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
-        {
+        {            
             status.Content = "X: " + e.GetPosition(main).X + " Y: " + e.GetPosition(main).Y;
-        }
+        }                
     }
-}
+ }
